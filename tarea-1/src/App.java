60bbe0b3168ac41f074a6fdc0323cc5230495934
jmella22 <ing.jose.mella@gmail.com>
2022-07-05 00:59:30 -0400

    primera tarea terminada

--- tarea-1/src/App.java
+++ tarea-1/src/App.java
@@ -1,5 +1,28 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+
+    int resultado = suma(10 ,20,30);
+
+    System.out.println(resultado);
+
+    Coche miCoche = new Coche();
+
+    miCoche.agregarpuerta();
+
+    System.out.println(miCoche.puertas);
+  }
+
+  public static int suma (int a,int b, int c){
+    return a + b + c;
+  }
+
 }
+
+class Coche{
+  public int puertas =0;
+
+  public void agregarpuerta(){
+    this.puertas++;
+  }
+}
+
